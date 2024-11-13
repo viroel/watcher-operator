ed1e612d8379df6594e120baf10ffa9a501887e8
Alfredo Moralejo <amoralej@redhat.com>
2024-11-13 09:23:28 +0100

    Create empty WatcherDecisionEngine CRD and controller
    Intended to manage de watcher-decision-engine component of watcher
    service.
    
     Using openstack-sdk:
    
    $ operator-sdk create api \
                          --group watcher \
                          --version v1beta1 \
                          --kind WatcherDecisionEngine \
                           --resource \
                           --controller
    
    JIRA: OSPRH-11484

--- main.go
+++ main.go
@@ -136,6 +136,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Watcher")
 		os.Exit(1)
 	}
+	if err = (&controllers.WatcherDecisionEngineReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "WatcherDecisionEngine")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
